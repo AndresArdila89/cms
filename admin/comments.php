39a87ee8bb2b1425235f521ff1a8cd6d6b17cc84
AndresArdila89 <andresardila@me.com>
2020-08-21 13:32:27 -0400

    view all comments file needs to be able to delet edit and approve the comments

---
+++ admin/comments.php
@@ -0,0 +1,52 @@
+<?php include "includes/admin_header.inc.php" ?>
+<?php include "includes/admin_navigation.inc.php" ?>
+
+
+<div id="page-wrapper">
+  <div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="row">
+      <div class="col-lg-12">
+        <h1 class="page-header">Posts <small>edit a post</small></h1>
+
+
+        <?php
+
+        if (isset($_GET['source'])) {
+            $source = $_GET['source'];
+        } else {
+            $source = '';
+        }
+
+        switch ($source) {
+        case 'add_post':
+        include "includes/add_post.inc.php";
+        break;
+
+        case 'edit_post':
+        include "includes/edit_post.inc.php";
+        break;
+
+        case '122':
+        echo "nice";
+        break;
+
+        default:
+        include "includes/view_all_comments.inc.php";
+        break;
+
+      }
+        ?>
+
+
+      </div>
+      <!-- col -->
+    </div>
+    <!-- /.row -->
+  </div>
+  <!-- /.container-fluid -->
+</div>
+<!-- /#page-wrapper -->
+
+<?php include "includes/admin_footer.inc.php" ?>
---
+++ admin/includes/view_all_comments.inc.php
@@ -0,0 +1,75 @@
+<table class="table table-bordered table-hover">
+  <thead>
+    <tr>
+      <th>Id</th>
+      <th>Author</th>
+      <th>Comment</th>
+      <th>email</th>
+      <th>Status</th>
+      <th>In Response to</th>
+      <th>Date</th>
+      <th>Approve</th>
+      <th>Unapprove</th>
+      <th>Delete</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+    if (isset($_GET['delete'])) {
+        $post_id = $_GET['delete'];
+        $query = "DELETE FROM comments WHERE comment_id = {$comment_id}";
+        $delete_comment_query = mysqli_query($connection, $query);
+
+        confirm_query($delete_comment_query);
+    }
+     ?>
+
+
+      <?php
+        $query = "SELECT * FROM comments";
+        $select_comments = mysqli_query($connection, $query);
+
+        if (!$select_comments) {
+            die("QUERY FAILED LOAD COMMENTS" .  mysqli_error($connection));
+        } else {
+            while ($row = mysqli_fetch_assoc($select_comments)) {
+                $id = $row['comment_id'];
+                $comment_post_id = $row['comment_post_id'];
+                $author = $row['comment_author'];
+                $email = $row['comment_email'];
+                $content = $row['comment_content'];
+                $status = $row['comment_status'];
+                $date = $row['comment_date'];
+
+
+
+
+
+
+                echo "<tr>";
+                echo "<td>$id</td>";
+                echo "<td>$author</td>";
+                echo "<td>$email</td>";
+
+                // $query = "SELECT * FROM categories WHERE cat_id = $category";
+                // $select_categorie = mysqli_query($connection, $query);
+                // while ($row = mysqli_fetch_assoc($select_categorie)) {
+                //     $cat_id= $row['cat_id'];
+                //     $cat_title = $row['cat_title'];
+                //     echo "<td>$cat_title</td>";
+                // }
+
+                echo "<td>$content</td>";
+                echo "<td>$status</td>";
+                echo "<td>$date</td>";
+                echo "<td><a href='posts.php?source=edit_post&post_id={$id}'>Approve</a></td>";
+                echo "<td><a href='posts.php?delete={$id}'>Unapprove</a></td>";
+                echo "<td><a href='posts.php?source=edit_post&post_id={$id}'>Edit</a></td>";
+                echo "<td><a href='posts.php?delete={$id}'>Delete</a></td>";
+                echo "</tr>";
+            }
+        }
+
+       ?>
+  </tbody>
+</table>
